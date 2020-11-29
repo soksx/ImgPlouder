b8246527d6bbb9336968b9e4fa92bf5b1f099358
soksx <ivandelcura@hotmail.com>
2020-11-29 10:53:16 +0100

    Missed Commits

--- ImgPlouder.Common/Core/Domain/Interfaces/IImgPlouderImg.cs
+++ ImgPlouder.Common/Core/Domain/Interfaces/IImgPlouderImg.cs
@@ -3,5 +3,14 @@ namespace ImgPlouder.Common.Core.Domain.Interfaces
 {
     public interface IImgurImage
     {
+        /// <summary>
+        ///     OPTIONAL, the deletehash, if you're logged in as the image owner.
+        /// </summary>
+        string DeleteHash { get; set; }
+        /// <summary>
+        ///     The direct link to the the image. (Note: if fetching an animated GIF that was over 20MB in original size, a .gif
+        ///     thumbnail will be returned)
+        /// </summary>
+        string Link { get; set; }
     }
 }
